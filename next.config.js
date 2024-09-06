a1fde317c5d84a17ecffaae76c3f9c4d6dfa26cc
jollygrin <JollyGrin@protonmail.com>
2024-09-06 04:05:00 +0200

    remove prefix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const pathPrefix = process.env.NODE_ENV === "production" ? "" : "";
-
-console.log("Environment:", process.env.NODE_ENV);
 const nextConfig = {
   output: "export",
-  assetPrefix: pathPrefix,
-  env: {
-    pathPrefix,
-  },
   reactStrictMode: true,
 };
 
