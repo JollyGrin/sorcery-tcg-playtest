91f2ee919105eb90ff94c931ceaea48f3b743fda
jollygrin <JollyGrin@protonmail.com>
2024-09-06 03:59:57 +0200

    custom domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const pathPrefix =
-  process.env.NODE_ENV === "production" ? "/sorcery-tcg-playtest" : "";
+const pathPrefix = process.env.NODE_ENV === "production" ? "" : "";
 
 console.log("Environment:", process.env.NODE_ENV);
 const nextConfig = {
