37707bd8764960aa066fe86588fd9cfac2505189
jollygrin <JollyGrin@protonmail.com>
2024-09-08 12:31:45 +0200

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import { CardAtlas } from "@/components/atoms/mock-cards/atlas";
-import { DroppableGridItem } from "@/components/molecules/DropGridItem";
-import { Grid } from "styled-system/jsx";
-import { grid } from "styled-system/patterns";
-import { DndContext, DragEndEvent, useDraggable } from "@dnd-kit/core";
-import { ReactNode, useEffect } from "react";
-import { LAYOUT_HEIGHTS } from "@/components/organisms/GameBoard/constants";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-
-const { nav, body, footer } = LAYOUT_HEIGHTS;
 
 export default function Home() {
   const router = useRouter();
