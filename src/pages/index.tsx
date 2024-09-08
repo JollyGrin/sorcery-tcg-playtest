9822b183135569c8a3d615cc1674fb08f5dcae4e
jollygrin <JollyGrin@protonmail.com>
2024-09-08 02:20:18 +0200

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,19 @@ import { DroppableGridItem } from "@/components/molecules/DropGridItem";
 import { Grid } from "styled-system/jsx";
 import { grid } from "styled-system/patterns";
 import { DndContext, DragEndEvent, useDraggable } from "@dnd-kit/core";
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { LAYOUT_HEIGHTS } from "@/components/organisms/GameBoard/constants";
+import { useRouter } from "next/router";
 
 const { nav, body, footer } = LAYOUT_HEIGHTS;
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/game");
+  }, []);
+  return null;
+
   const [, setIsDropped] = useState(false);
 
   function handleDragEnd(event: DragEndEvent) {
