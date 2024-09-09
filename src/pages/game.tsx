a5672a16abd77bc0252da4f817697644856a6adf
jollygrin <JollyGrin@protonmail.com>
2024-09-09 02:49:58 +0200

    add cards

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -5,24 +5,9 @@ import { useState } from "react";
 
 const handCards: GameCard[] = [
   {
-    id: "atlas-02",
-    img: "atlas_cloud_city.webp",
-    type: "site",
-  },
-  {
-    id: "headless-02",
-    img: "headless_haunt.webp",
-    type: "minion",
-  },
-  {
-    id: "headless-03",
-    img: "headless_haunt.webp",
-    type: "minion",
-  },
-  {
-    id: "headless-04",
-    img: "headless_haunt.webp",
-    type: "minion",
+    id: "sorcerer-01",
+    img: "sorcerer.webp",
+    type: "avatar",
   },
 ];
 
@@ -32,18 +17,45 @@ const deckCards = Array.from({ length: 30 }).map((_, index) => ({
   type: "minion",
 }));
 
+const mockDeck: GameCard[] = [
+  {
+    id: "jihad",
+    img: "jihad.webp",
+    type: "aura",
+  },
+  {
+    id: "death_dealer-01",
+    img: "death_dealer.webp",
+    type: "minion",
+  },
+  {
+    id: "infernal_legion-01",
+    img: "infernal_legion.webp",
+    type: "minion",
+  },
+  {
+    id: "wayfaring_pilgrim-01",
+    img: "wayfaring_pilgrim.webp",
+    type: "minion",
+  },
+];
+
 const atlasCards = Array.from({ length: 10 }).map((_, index) => ({
   id: "atlas-02" + index,
-  img: "atlas_cloud_city.webp",
+  img: "arid_desert.webp",
   type: "site",
 }));
 
 const initCards: GameCard[][] = Array.from({ length: 35 }, () => []);
 initCards[GRIDS.HAND] = handCards as GameCard[];
-initCards[GRIDS.DECK] = deckCards as GameCard[];
+initCards[GRIDS.DECK] = [...deckCards, ...mockDeck] as GameCard[];
 initCards[GRIDS.ATLAS_DECK] = atlasCards as GameCard[];
 
-export type GameCard = SorceryCard & { id: string }; // for game position
+type GameProps = {
+  id: string;
+  isTapped?: boolean;
+};
+export type GameCard = SorceryCard & GameProps; // for game position
 export default function GamePage() {
   const [gridItems, setGridItems] = useState<GameCard[][]>(initCards);
 
