7dc133c8cf9f9306c4db1271e63465028b5e1dd5
jollygrin <JollyGrin@protonmail.com>
2024-09-12 02:49:59 +0200

    add p1 p2 toggle

--- src/components/organisms/GameBoard/Layout.tsx
+++ src/components/organisms/GameBoard/Layout.tsx
@@ -5,6 +5,7 @@ import { LAYOUT_HEIGHTS } from "./constants";
 import { GameFooter } from "./Footer";
 import { GameStateActions } from ".";
 import { Auras } from "./Auras";
+import Link from "next/link";
 
 const { nav, body, footer } = LAYOUT_HEIGHTS;
 
@@ -18,11 +19,20 @@ export const GameLayout = (
           background: "rgba(0,200,0,0.2)",
           padding: "0.5rem",
           height: nav,
+          display: "flex",
+          gap: "2rem",
         }}
       >
         <p style={{ width: "fit-content" }}>
           Playtest cards on a grid. Click to tap. Right click to view big.
         </p>
+        <Link href={{ query: { name: "p1" } }}>
+          <p>p1</p>
+        </Link>
+
+        <Link href={{ query: { name: "p2" } }}>
+          <p>p2</p>
+        </Link>
       </div>
       <Box position="relative" h="100%" w="100%" maxW="1200px" m="0 auto">
         <Auras {...props} />
