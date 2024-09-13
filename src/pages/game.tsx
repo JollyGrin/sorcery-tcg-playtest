d5f6bc949ae35a989dbf7da47bca0fae22a838bc
jollygrin <JollyGrin@protonmail.com>
2024-09-13 18:13:36 +0200

    remove logs

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -22,10 +22,8 @@ export default function GamePage() {
     return (state: GameState) => {
       const newState = [...state]; // make a copy of state
       const GLOBAL = newState.slice(0, GRIDS.AURA_12 + 1); // GLOBAL takes game grid
-      console.log("lastGlobal", GLOBAL[GLOBAL.length - 1]);
       const GLOBAL_EMPTY = Array.from({ length: 4 }, () => []); // empties player data
       const newGlobal = [...GLOBAL, ...GLOBAL_EMPTY];
-      console.log("newGlobal", newGlobal, newGlobal.length);
       setPlayers((prev) => ({
         ...prev,
         GLOBAL: newGlobal,
@@ -35,37 +33,11 @@ export default function GamePage() {
   }
 
   const state = useMemo(() => {
-    console.log("global", players.GLOBAL.slice(0, GRIDS.AURA_12));
-    console.log(
-      "perosnal",
-      players[name as keyof typeof players].slice(GRIDS.AURA_12),
-    );
     return [
       ...players.GLOBAL.slice(0, GRIDS.HAND),
       ...players[name as keyof typeof players].slice(GRIDS.HAND),
     ];
   }, [players, name]);
-
-  // const arrayOfObjects = state.map((row) => {
-  //   return row.reduce((acc, obj, index) => {
-  //     acc[`Column ${index}`] = obj.id; // Extract the `id` property from each object
-  //     return acc;
-  //   }, {});
-  // });
-
-  // Transform each row into an object that includes the `id` from each object
-  const arrayOfObjects = state.map((row, rowIndex) => {
-    // Start with the row name from the enum
-    const rowObject = { "Row Name": GRIDS[rowIndex] };
-
-    // Add the id from each object in the row
-    row.forEach((obj, colIndex) => {
-      rowObject[`Card Index ${colIndex}`] = obj.id;
-    });
-
-    return rowObject;
-  });
-  console.table(arrayOfObjects);
 
   if (needsDeck(players["p1"]))
     return (
