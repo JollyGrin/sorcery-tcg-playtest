9b2e5c14ab9a8e379e6801efbfd470105fb9af8d
jollygrin <JollyGrin@protonmail.com>
2024-09-14 14:56:43 +0200

    add hook

--- src/utils/api/curiosa/useCuriosa.ts
+++ src/utils/api/curiosa/useCuriosa.ts
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCuriosaDeck } from "./api";
+import { getCuriosaDeck, getRealmsAppDeck } from "./api";
 
 export const useCuriosaDeck = (deckId?: string) => {
   return useQuery({
     queryKey: ["deck:curiosa", deckId],
     queryFn: () => getCuriosaDeck(deckId as string),
+    enabled: !!deckId,
+  });
+};
+
+export const useRealmsAppDeck = (deckId?: string) => {
+  return useQuery({
+    queryKey: ["deck:realms", deckId],
+    queryFn: () => getRealmsAppDeck(deckId as string),
     enabled: !!deckId,
   });
 };
