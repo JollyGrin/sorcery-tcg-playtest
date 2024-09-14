15e84ceb0d6dbe8e3dec2661e5166f043b01bac7
jollygrin <JollyGrin@protonmail.com>
2024-09-14 12:48:26 +0200

    add uuids to prevent two cards sharing same id

--- src/components/molecules/LoadDeck/index.tsx
+++ src/components/molecules/LoadDeck/index.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import { Box, Flex } from "styled-system/jsx";
 import { button, input } from "styled-system/recipes";
 
+import { v4 as uuid } from "uuid";
+
 export const LoadDeck = (props: GameStateActions) => {
   const [deckId, setDeckId] = useState<string>("");
   const { data: deck } = useCuriosaDeck(deckId);
@@ -27,7 +29,7 @@ export const LoadDeck = (props: GameStateActions) => {
         })).map(
           (rest, index) =>
             ({
-              id: rest.identifier + index + Date.now(),
+              id: rest.identifier + index + uuid(),
               img: rest.identifier,
               type: "site",
             }) as GameCard,
@@ -42,7 +44,7 @@ export const LoadDeck = (props: GameStateActions) => {
         })).map(
           (rest, i) =>
             ({
-              id: rest.identifier + i + Date.now(),
+              id: rest.identifier + uuid(),
               img: rest.identifier,
               type: "minion",
             }) as GameCard,
@@ -53,7 +55,7 @@ export const LoadDeck = (props: GameStateActions) => {
     newGrid[GRIDS.HAND] = deck?.avatar?.map(
       (avatar) =>
         ({
-          id: avatar.identifier,
+          id: avatar.identifier + "-avatar-" + uuid(),
           img: avatar.identifier,
           type: "avatar",
         }) as GameCard,
