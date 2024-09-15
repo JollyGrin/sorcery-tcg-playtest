e81c5fce93e4338ed677bb57655d360c39c7be56
jollygrin <JollyGrin@protonmail.com>
2024-09-15 23:23:08 +0200

    add github link and readme

--- src/components/organisms/LandingPage/Nav.tsx
+++ src/components/organisms/LandingPage/Nav.tsx
@@ -1,9 +1,12 @@
-import { Box } from "styled-system/jsx";
+import Link from "next/link";
+import { Box, HStack } from "styled-system/jsx";
 
 export const Nav = () => {
   return (
-    <Box color="white">
-      <p>nav</p>
-    </Box>
+    <HStack color="white" justifyContent="end">
+      <Link href={"https://github.com/JollyGrin/sorcery-tcg-playtest"}>
+        <p>Github</p>
+      </Link>
+    </HStack>
   );
 };
