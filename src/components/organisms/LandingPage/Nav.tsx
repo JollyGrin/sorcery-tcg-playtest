98fdfe452cc97fde22eefca17e5f5a57ad8fbb13
jollygrin <JollyGrin@protonmail.com>
2024-09-18 23:39:17 +0200

    update gap

--- src/components/organisms/LandingPage/Nav.tsx
+++ src/components/organisms/LandingPage/Nav.tsx
@@ -3,9 +3,15 @@ import { HStack } from "styled-system/jsx";
 
 export const Nav = () => {
   return (
-    <HStack color="white" justifyContent="end" minH="3rem">
+    <HStack color="white" justifyContent="end" minH="3rem" gap={8}>
       <Link href={"https://github.com/JollyGrin/sorcery-tcg-playtest"}>
         <p>Github</p>
+      </Link>
+
+      <Link
+        href={"https://github.com/JollyGrin/sorcery-tcg-playtest/issues/new"}
+      >
+        <p>Report a Bug/Request Feature</p>
       </Link>
     </HStack>
   );
