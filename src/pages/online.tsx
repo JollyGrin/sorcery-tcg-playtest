d3559b3c11a7ba44a41c471bf8de33966770cb89
jollygrin <JollyGrin@protonmail.com>
2024-09-18 03:13:40 +0200

    its working

--- src/pages/online.tsx
+++ src/pages/online.tsx
@@ -1,116 +1,161 @@
+/**
+ * ALMOST DONE
+ * TODO: add a way to ensure the 2nd player rotates their screen
+ * */
+
+import { LoadDeck } from "@/components/molecules/LoadDeck";
 import { GameBoard } from "@/components/organisms/GameBoard";
 import {
   GRIDS,
   initGameData,
   initGameState,
 } from "@/components/organisms/GameBoard/constants";
-import { CreateLobby } from "@/components/organisms/Online/CreateLobby";
-import { WebGameProvider, useWebGame } from "@/lib/contexts/WebGameProvider";
-import { GameState, PlayerData, PlayerState, PlayersState } from "@/types/card";
+import { useWebGame, WebGameProvider } from "@/lib/contexts/WebGameProvider";
+import { useCreateLobby } from "@/lib/hooks";
+import { GameState, PlayerData, PlayersState, PlayerState } from "@/types/card";
 import { useRouter } from "next/router";
-import { ReactNode, useEffect, useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+import { Box } from "styled-system/jsx";
 import { button, input } from "styled-system/recipes";
 
-export default function OnlinePage() {
+export default function WebsocketDebug() {
   const { query } = useRouter();
   const { name, gid } = query;
-
   if (!name || !gid) return <CreateLobby />;
 
   return (
     <WebGameProvider>
-      {/* {name && ( */}
-      {/*   <Init name={name as string}> */}
-      {/*     ds */}
-      {/*     <Online name={name as string} /> */}
-      {/*   </Init> */}
-      {/* )} */}
-
-      {name && <Online name={name as string} />}
+      <Body />
     </WebGameProvider>
   );
 }
 
-const Init = (props: {
-  name: string;
-  setState(state: GameState): void;
-  setData(data: PlayerData): void;
-}) => {
+const CreateLobby = () => {
+  const { push, query } = useRouter();
+  const [fields, setFields] = useState({ name: "", gid: "" });
+  const gidRef = useRef(null);
+  const nameRef = useRef(null);
+  const { refetch, loading, setLoading, disclosure } = useCreateLobby({
+    gidRef,
+    nameRef,
+  });
+  function onSubmit() {
+    push({ query: { ...fields } });
+    refetch(undefined);
+  }
   return (
-    <button
-      className={button()}
-      onClick={() => {
-        props.setState(initGameState);
-      }}
-    >
-      Init Data
-    </button>
+    <Box maxW="500px" m="0 auto" mt="10rem">
+      <input
+        ref={nameRef}
+        placeholder="name"
+        onChange={(e) =>
+          setFields((prev) => ({
+            ...prev,
+            name: e.target.value,
+          }))
+        }
+        className={input()}
+      />
+
+      <input
+        ref={gidRef}
+        placeholder="gid"
+        onChange={(e) =>
+          setFields((prev) => ({
+            ...prev,
+            gid: e.target.value,
+          }))
+        }
+        className={input()}
+      />
+      <button className={button()} onClick={onSubmit}>
+        submit
+      </button>
+    </Box>
   );
 };
 
-const Online = (props: { name: string }) => {
-  const { gameState: socketState, setPlayerState: setMySocketState } =
-    useWebGame();
+const Body = () => {
+  const { query } = useRouter();
+  const name = query.name as string | undefined;
+  const { gameState, setPlayerState } = useWebGame();
+  const myState = gameState?.content?.players?.[name ?? ""];
 
-  const socketPlayers = socketState?.content?.players as Record<
+  function setState(state: GameState) {
+    setPlayerState()({
+      state,
+      data: myState?.data ?? initGameData,
+      timestamp: Date.now(),
+    });
+  }
+
+  function setData(data: PlayerData) {
+    setPlayerState()({
+      state: myState?.state ?? initGameState,
+      data,
+      timestamp: Date.now(),
+    });
+  }
+
+  const noState = Object.keys(myState ?? {}).length === 0;
+  const isEmpty = noState || myState?.state?.[GRIDS.DECK]?.length === 0;
+
+  if (myState?.state === undefined && name) {
+    return (
+      <LoadDeck
+        playerName={name}
+        gridItems={myState?.state ?? initGameState}
+        setGridItems={(state: GameState) => {
+          setPlayerState()({
+            state,
+            data: myState?.data ?? initGameData,
+            timestamp: Date.now(),
+            joinTimestamp: Date.now(),
+          });
+        }}
+      >
+        pick one
+      </LoadDeck>
+    );
+  }
+
+  if (myState?.state === undefined) return null;
+
+  const socketPlayers = gameState?.content?.players as Record<
     string,
     PlayerState
   >;
-  const mySocketState = socketPlayers?.[props.name] as PlayerState;
 
-  function setPlayerState(state: GameState) {
-    setMySocketState()({ state, data: mySocketState?.data ?? initGameData });
-  }
+  const [firstJoiner] = Object.entries(socketPlayers).sort((a, b) => {
+    const [_, valueA] = a;
+    const [__, valueB] = b;
+    const timeA = valueA.joinTimestamp ?? 0;
+    const timeB = valueB.joinTimestamp ?? 0;
+    return timeA - timeB;
+  });
+  const [firstName] = firstJoiner;
+  const isReversed = name !== firstName;
 
-  function setPlayerData(data: PlayerState["data"]) {
-    setMySocketState()({ state: mySocketState.state ?? initGameState, data });
-  }
-
-  /**
-   * Combines the game state arrays (1-32) from all players into one merged state.
-   * The last updated player's state takes priority.
-   */
   function combineGameStates(): GameState {
     const playersState = socketPlayers;
-    // Create an empty array to hold the combined state for the first 32 slots.
-    const combinedState: GameState = Array.from({ length: 32 }, () => []);
-
-    if (!playersState) return combinedState;
-
-    // Iterate over each player, merging their state into the combined state.
-    Object.values(playersState).forEach((playerState) => {
-      const localState = playerState?.state ?? combinedState;
-      localState.slice(0, 32).forEach((cards, index) => {
-        combinedState[index] = cards; // Overwrite earlier player's state
-      });
-    });
-
-    return combinedState;
+    const [mostRecentState] = Object.values(playersState).sort(
+      (a, b) => (b.timestamp ?? 0) - (a.timestamp ?? 0),
+    );
+    return mostRecentState?.state?.slice(0, 32);
   }
 
   const state = useMemo(() => {
-    const localState = mySocketState?.state ?? initGameState;
+    const localState = myState?.state ?? initGameState;
     return [...combineGameStates(), ...localState.slice(GRIDS.HAND)];
   }, [socketPlayers]);
 
-  console.log({ mySocketState });
-  if (mySocketState?.data === undefined || mySocketState?.state === undefined)
-    return (
-      <Init
-        name={props.name}
-        setState={setPlayerState}
-        setData={setPlayerData}
-      />
-    );
-
   return (
-    <>
-      <GameBoard
-        players={socketPlayers as PlayersState}
-        gridItems={state}
-        setGridItems={setPlayerState}
-        setMyData={setPlayerData}
-      />
-    </>
+    <GameBoard
+      isReversed={isReversed}
+      players={socketPlayers as PlayersState}
+      gridItems={state}
+      setGridItems={setState}
+      setMyData={setData}
+    />
   );
 };
