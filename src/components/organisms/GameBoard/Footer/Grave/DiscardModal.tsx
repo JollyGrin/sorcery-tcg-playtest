f359858cb20f489268ea44b127a5ba1fc43afa63
jollygrin <JollyGrin@protonmail.com>
2024-09-29 17:23:43 +0200

    add toggle banish

--- src/components/organisms/GameBoard/Footer/Grave/DiscardModal.tsx
+++ src/components/organisms/GameBoard/Footer/Grave/DiscardModal.tsx
@@ -7,16 +7,24 @@ import { PiHandWithdrawFill as IconHand } from "react-icons/pi";
 import { useHover } from "@/utils/hooks/useHover";
 import { useRef } from "react";
 import { GameStateActions } from "../..";
-import { actDrawDiscard } from "@/utils/actions/discard";
+import { actDrawDiscard, actToggleBanishCard } from "@/utils/actions/discard";
 
 export const DiscardModalBody = (
   props: {
     cards: GridItem;
-  } & GameStateActions,
+  } & Partial<GameStateActions>,
 ) => {
   function returnToHand(cardIndex: number) {
+    if (!props.setGridItems || !props.gridItems) return;
     props.setGridItems(actDrawDiscard(props.gridItems, cardIndex));
   }
+
+  function toggleBanish(cardIndex: number) {
+    if (!props.setGridItems || !props.gridItems) return;
+    props.setGridItems(actToggleBanishCard(props.gridItems, cardIndex));
+  }
+
+  const hasFunctions = !!props.setGridItems && !!props.gridItems;
 
   return (
     <Grid
@@ -27,13 +35,22 @@ export const DiscardModalBody = (
       overflowX="clip"
       overflowY="scroll"
     >
-      {props.cards?.map((card, index) => (
-        <Card
-          key={card.id}
-          card={card}
-          returnToHand={() => returnToHand(index)}
-        />
-      ))}
+      {props.cards
+        ?.sort((a, b) => {
+          if (a.isBanished && b.isBanished) {
+            return 0; // no sorting if both are the same
+          }
+          return a.isBanished ? 1 : -1; // move banished cards to the bottom
+        })
+        ?.map((card, index) => (
+          <Card
+            key={card.id}
+            card={card}
+            returnToHand={() => returnToHand(index)}
+            toggleBanish={() => toggleBanish(index)}
+            hasFunctions={hasFunctions}
+          />
+        ))}
     </Grid>
   );
 };
@@ -41,9 +58,13 @@ export const DiscardModalBody = (
 const Card = ({
   card,
   returnToHand,
+  toggleBanish,
+  hasFunctions,
 }: {
   card: GameCard;
   returnToHand(): void;
+  toggleBanish(): void;
+  hasFunctions: boolean;
 }) => {
   const hoverRef = useRef(null);
   const isHovering = useHover(hoverRef);
@@ -57,29 +78,54 @@ const Card = ({
       h="100%"
       maxH="500px"
       position="relative"
-      filter="saturate(1)"
+      filter={card.isBanished ? "saturate(0)" : "saturate(1)"}
       _hover={{
-        filter: "saturate(1.75)",
+        filter: card.isBanished ? "" : "saturate(1.75)",
       }}
     >
       <CardImage img={card.img} minH={"0"} />
-      <button
-        onClick={returnToHand}
-        className={button()}
-        style={{
-          position: "absolute",
-          bottom: "50%",
-          right: "calc(50% - 50px)",
-          opacity: isHovering ? 1 : 0.1,
-          width: "100px",
-          transition: "all 0.25s ease",
-        }}
+      <HStack
+        display={hasFunctions ? "flex" : "none"}
+        position="absolute"
+        bottom="50%"
+        right="calc(50% - 100px)"
       >
-        <HStack gap={0}>
-          <p>Return</p>
-          <IconHand size="2rem" style={{ fontSize: "2rem", color: "white" }} />
-        </HStack>
-      </button>
+        <button
+          onClick={returnToHand}
+          className={button()}
+          style={{
+            opacity: isHovering ? 1 : 0.1,
+            width: "100px",
+            transition: "all 0.25s ease",
+          }}
+        >
+          <HStack gap={0}>
+            <p>Return</p>
+            <IconHand
+              size="2rem"
+              style={{ fontSize: "2rem", color: "white" }}
+            />
+          </HStack>
+        </button>
+
+        <button
+          onClick={toggleBanish}
+          className={button()}
+          style={{
+            opacity: isHovering ? 1 : 0.1,
+            width: "100px",
+            transition: "all 0.25s ease",
+          }}
+        >
+          <HStack gap={0}>
+            <p>Toggle Banish</p>
+            <IconHand
+              size="2rem"
+              style={{ fontSize: "2rem", color: "white" }}
+            />
+          </HStack>
+        </button>
+      </HStack>
     </Box>
   );
 };
