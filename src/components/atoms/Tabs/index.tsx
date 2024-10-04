523d4bc359daa3e535f1b62156124ed07fa5a428
jollygrin <JollyGrin@protonmail.com>
2024-10-04 22:54:47 +0200

    prepare

--- src/components/atoms/Tabs/index.tsx
+++ src/components/atoms/Tabs/index.tsx
@@ -8,20 +8,29 @@ import { ReactNode } from "react";
 
 type Props = {
   tabs: ReactNode[];
-  content: ReactNode[];
+  content?: ReactNode[];
+  onSelect?(index: number): void;
 };
 
-export const Tabs = ({ tabs, content }: Props) => {
+export const Tabs = ({
+  tabs,
+  content,
+  onSelect = (_: number) => {},
+}: Props) => {
   return (
     <TabsWrapper defaultValue="0">
       <TabsList>
         {tabs.map((tab, index) => (
-          <TabsTrigger key={index + "trigger"} value={index.toString()}>
+          <TabsTrigger
+            key={index + "trigger"}
+            value={index.toString()}
+            onClick={() => onSelect(index)}
+          >
             {tab}
           </TabsTrigger>
         ))}
       </TabsList>
-      {content.map((content, index) => (
+      {content?.map((content, index) => (
         <TabsContent key={"content" + index} value={index.toString()}>
           {content}
         </TabsContent>
--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -1,10 +1,7 @@
+import { DraftBoard } from "@/components/organisms/Draft";
 import { DraftCard } from "@/components/organisms/Draft/Card";
 import { Grid } from "styled-system/jsx";
 
 export default function DraftPage() {
-  return (
-    <Grid>
-      <DraftCard />
-    </Grid>
-  );
+  return <DraftBoard />;
 }
