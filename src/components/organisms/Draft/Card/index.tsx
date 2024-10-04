6cb331471378751eeefdb9f932a0b1ef6a8b6eed
jollygrin <JollyGrin@protonmail.com>
2024-10-04 16:58:29 +0200

    setup

---
+++ src/components/organisms/Draft/Card/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from "styled-system/jsx";
+
+export const DraftCard = () => {
+  return (
+    <Box bg="red" w="100px" h="180px">
+      Hi
+    </Box>
+  );
+};
---
+++ src/pages/draft/index.tsx
@@ -0,0 +1,10 @@
+import { DraftCard } from "@/components/organisms/Draft/Card";
+import { Grid } from "styled-system/jsx";
+
+export default function DraftPage() {
+  return (
+    <Grid>
+      <DraftCard />
+    </Grid>
+  );
+}
