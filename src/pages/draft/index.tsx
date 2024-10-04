ff1ead6958cbbc58068f6e4f466cf443740917b7
jollygrin <JollyGrin@protonmail.com>
2024-10-04 23:55:26 +0200

    setup types

--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -1,5 +1,49 @@
 import { DraftBoard } from "@/components/organisms/Draft";
+import { CardDTO } from "@/utils/api/cardData/CardDataType";
+import { useCardFullData } from "@/utils/api/cardData/useCardData";
+import { useState } from "react";
+
+type BoosterPack = {
+  uuid: string;
+  playerName: string;
+  cards: CardDTO[];
+};
+type DraftPlayerData = {
+  selectedCards: CardDTO[];
+  activePack: CardDTO[];
+  finishedPacks: CardDTO[][];
+};
 
 export default function DraftPage() {
+  const [players, setPlayers] = useState<Record<string, DraftPlayerData>>({
+    p1: {
+      selectedCards: [],
+      activePack: [],
+      finishedPacks: [[], [], []],
+    },
+  });
+
+  const { data: cardData = [] } = useCardFullData();
+  function generateNewBooster() {
+    const arr = [...cardData];
+    const count = 15;
+    let shuffled = arr.slice(); // Shallow copy to avoid mutating the original array
+    for (let i = arr.length - 1; i > arr.length - 1 - count; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+    }
+    return shuffled.slice(-count); // Return the last `count` items
+  }
+
+  function setPlayer(data: (typeof players)[number]) {
+    return setPlayers((prev) => ({
+      ...prev,
+      p1: {
+        ...prev["p1"],
+        data,
+      },
+    }));
+  }
+
   return <DraftBoard />;
 }
---
+++ src/utils/api/cardData/CardDataType.ts
@@ -0,0 +1,55 @@
+type Thresholds = {
+  air: number;
+  earth: number;
+  fire: number;
+  water: number;
+};
+
+type Guardian = {
+  rarity: string;
+  type: string;
+  typeText: string;
+  subType: string;
+  rulesText: string;
+  cost: number;
+  attack: number;
+  defence: number;
+  life: number | null;
+  thresholds: Thresholds;
+};
+
+type Variant = {
+  finish: string;
+  product: string;
+  artist: string;
+  flavorText: string;
+};
+
+type SetMetadata = {
+  rarity: string;
+  type: string;
+  typeText: string;
+  subType: string;
+  rulesText: string;
+  cost: number;
+  attack: number;
+  defence: number;
+  life: number | null;
+  thresholds: Thresholds;
+};
+
+type CardSet = {
+  name: string;
+  slug: string;
+  releasedAt: string;
+  type: string;
+  variants: Variant[];
+  metadata: SetMetadata;
+};
+
+export type CardDTO = {
+  name: string;
+  slug: string;
+  guardian: Guardian;
+  sets: CardSet[];
+};
