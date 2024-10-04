a6dd84a52cb7df5bfa55bed6cc645b588015c170
jollygrin <JollyGrin@protonmail.com>
2024-10-05 00:11:03 +0200

    prepare the wiring

--- src/components/organisms/Draft/index.tsx
+++ src/components/organisms/Draft/index.tsx
@@ -2,13 +2,41 @@ import { DraftCard } from "@/components/organisms/Draft/Card";
 import { Tabs } from "@/components/atoms/Tabs";
 import { Box, Grid, HStack } from "styled-system/jsx";
 import { Button } from "@/components/ui/button";
+import { DraftPlayerData } from "./types";
+import { useCardFullData } from "@/utils/api/cardData/useCardData";
 
 const hTop = "20vh";
 const hTabs = "5vh";
 const hCards = "75vh";
 export const gridHeight = { top: hTop, tabs: hTabs, cards: hCards };
 
-export const DraftBoard = () => {
+export const DraftBoard = ({
+  player,
+  setPlayerData,
+}: {
+  player: DraftPlayerData;
+  setPlayerData(data: DraftPlayerData): void;
+}) => {
+  const { data: cardData = [] } = useCardFullData();
+
+  function generateNewBooster() {
+    const arr = [...cardData];
+    const count = 15;
+    let shuffled = arr.slice(); // Shallow copy to avoid mutating the original array
+    for (let i = arr.length - 1; i > arr.length - 1 - count; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+    }
+    return shuffled.slice(-count); // Return the last `count` items
+  }
+
+  function crackBooster() {
+    const newBooster = generateNewBooster();
+    setPlayerData({
+      ...player,
+    });
+  }
+
   return (
     <Grid
       h="100vh"
--- src/pages/draft/index.tsx
+++ src/pages/draft/index.tsx
@@ -1,49 +1,27 @@
 import { DraftBoard } from "@/components/organisms/Draft";
-import { CardDTO } from "@/utils/api/cardData/CardDataType";
+import { DraftPlayerData } from "@/components/organisms/Draft/types";
 import { useCardFullData } from "@/utils/api/cardData/useCardData";
 import { useState } from "react";
-
-type BoosterPack = {
-  uuid: string;
-  playerName: string;
-  cards: CardDTO[];
-};
-type DraftPlayerData = {
-  selectedCards: CardDTO[];
-  activePack: CardDTO[];
-  finishedPacks: CardDTO[][];
-};
 
 export default function DraftPage() {
   const [players, setPlayers] = useState<Record<string, DraftPlayerData>>({
     p1: {
+      joinedSessionTimestamp: 1,
       selectedCards: [],
       activePack: [],
-      finishedPacks: [[], [], []],
+      pendingPacks: [[]],
+      finishedPacks: [[]],
     },
   });
 
-  const { data: cardData = [] } = useCardFullData();
-  function generateNewBooster() {
-    const arr = [...cardData];
-    const count = 15;
-    let shuffled = arr.slice(); // Shallow copy to avoid mutating the original array
-    for (let i = arr.length - 1; i > arr.length - 1 - count; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
-    }
-    return shuffled.slice(-count); // Return the last `count` items
-  }
-
-  function setPlayer(data: (typeof players)[number]) {
+  function setPlayer(data: DraftPlayerData) {
     return setPlayers((prev) => ({
       ...prev,
       p1: {
-        ...prev["p1"],
         data,
       },
     }));
   }
 
-  return <DraftBoard />;
+  return <DraftBoard player={players?.p1} setPlayerData={setPlayer} />;
 }
