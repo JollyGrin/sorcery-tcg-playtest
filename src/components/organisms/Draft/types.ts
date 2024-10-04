80f9713da7c7b19122eddb7df48edfb30747168c
jollygrin <JollyGrin@protonmail.com>
2024-10-05 00:10:57 +0200

    update types

--- src/components/organisms/Draft/types.ts
+++ src/components/organisms/Draft/types.ts
@@ -1,8 +1,16 @@
-export type DraftPlayer = {
+import { CardDTO } from "@/utils/api/cardData/CardDataType";
+
+export type BoosterPack = {
+  uuid: string;
+  playerName: string;
+  cards: CardDTO[];
+};
+
+export type DraftPlayerData = {
   joinedSessionTimestamp: number; // time of joining the session. Used for ordering
-  actions: {};
-  cards: {
-    selected: any[];
-    packs: any[][];
-  };
+  selectedCards: CardDTO[]; // cards you've picked
+  activePack: CardDTO[]; // pack actively picking from
+  pendingPacks: CardDTO[][]; // packs ready for pick (passed by other player)
+  finishedPacks: CardDTO[][]; // packs you've picked and ready to pass (ready to pass to other player)
+  deck?: CardDTO[]; // after draft, the deck you construct from selectedCards
 };
