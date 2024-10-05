967cc53020f925cfa0d4b95b6427cff2936ae8ee
jollygrin <JollyGrin@protonmail.com>
2024-10-05 17:09:17 +0200

    add arrows

--- src/components/organisms/Draft/Tray/index.tsx
+++ src/components/organisms/Draft/Tray/index.tsx
@@ -1,18 +1,42 @@
+import { Flex, HStack } from "styled-system/jsx";
 import { DraftPlayerData } from "../types";
+
+import { LuArrowBigRightDash as IconRight } from "react-icons/lu";
+import { RiArrowGoBackLine as IconReturnArrow } from "react-icons/ri";
 
 export const DraftTray = (props: {
   players: Record<string, DraftPlayerData>;
 }) => {
-  const players = Object.entries(props.players);
+  const players = Object.entries(props.players).sort((a, b) => {
+    const [, valueA] = a;
+    const [, valueB] = b;
+    return valueA.joinedSessionTimestamp - valueB.joinedSessionTimestamp;
+  });
+
   return (
-    <div data-testid="stats">
-      {players?.map(([key, value]) => {
+    <HStack data-testid="stats" p="1rem">
+      {players?.map(([key, value], index) => {
         return (
-          <p key={key}>
-            {key}:{value.joinedSessionTimestamp}
-          </p>
+          <Flex alignItems="center" gap={1}>
+            <Flex
+              key={key}
+              bg="lightblue"
+              p="1rem"
+              borderRadius="0.5rem"
+              border="solid 2px rgba(0,200,250,0.5)"
+            >
+              <p>
+                {key}:{value.joinedSessionTimestamp}
+              </p>
+            </Flex>
+            {index !== players.length - 1 ? (
+              <IconRight size="2rem" />
+            ) : (
+              <IconReturnArrow size="2rem" />
+            )}
+          </Flex>
         );
       })}
-    </div>
+    </HStack>
   );
 };
