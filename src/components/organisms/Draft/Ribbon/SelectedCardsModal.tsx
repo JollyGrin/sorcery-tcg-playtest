7317641fd50990d2add8f63ccb84201df74fd228
jollygrin <JollyGrin@protonmail.com>
2024-10-06 23:58:24 +0200

    setup bulk

--- src/components/organisms/Draft/Ribbon/SelectedCardsModal.tsx
+++ src/components/organisms/Draft/Ribbon/SelectedCardsModal.tsx
@@ -1,8 +1,9 @@
-import { Box, Grid } from "styled-system/jsx";
+import { Box, Flex, Grid } from "styled-system/jsx";
 import { Modal } from "@/components/atoms/Modal";
 import { getCardImage } from "../../GameBoard/constants";
 import { sortAlphabetical } from "./helpers";
 import { CardDTO } from "@/utils/api/cardData/CardDataType";
+import { Button } from "@/components/ui/button";
 
 export const SelectedCardsModal = ({
   cards = [],
@@ -18,16 +19,28 @@ export const SelectedCardsModal = ({
       <Modal
         wrapperProps={{ open: isOpen, onOpenChange: onToggle }}
         content={
-          <Grid gridTemplateColumns="5fr 1fr" minW="80vw" minH="50vh" w="100%">
-            <Grid gridTemplateColumns="repeat(auto-fit, minmax(16.4rem, 1fr))">
+          <Grid
+            gridTemplateColumns="4fr 1fr"
+            minW="70vw"
+            minH="50vh"
+            maxH="70vh"
+            w="100%"
+          >
+            <Flex
+              flexWrap="wrap"
+              gap="0.25rem"
+              h="100%"
+              overflowY="auto"
+              overflowX="clip"
+            >
               {cards.sort(sortAlphabetical).map((card, index) => (
                 <Box
                   key={card.slug + index}
-                  width="15rem"
+                  width="18rem"
                   height="fit-content"
                   transition="all 0.25s ease"
                   _hover={{
-                    transform: "scale(1.25)",
+                    transform: `scale(1.25) ${card.guardian.type === "Site" ? "rotate(90deg)" : ""}`,
                   }}
                 >
                   <img
@@ -38,11 +51,35 @@ export const SelectedCardsModal = ({
                   />
                 </Box>
               ))}
-            </Grid>
+            </Flex>
             <Box>
-              {cards.sort(sortAlphabetical).map((card, index) => {
-                return <p>{card.name}</p>;
-              })}
+              {cards
+                .sort(sortAlphabetical)
+                .reduce<{ name: string; count: number }[]>(
+                  (acc, card: Card) => {
+                    const existingCard = acc.find(
+                      (item) => item.name === card.name,
+                    );
+                    if (existingCard) {
+                      existingCard.count += 1;
+                    } else {
+                      acc.push({ name: card.name, count: 1 });
+                    }
+                    return acc;
+                  },
+                  [],
+                )
+                .map((card, index) => {
+                  return (
+                    <Grid gridTemplateColumns="1.5rem auto">
+                      <p>{card.count}x</p>
+                      <p>{card.name}</p>
+                    </Grid>
+                  );
+                })}
+              <Button mt="2rem" w="100%">
+                Copy Bulk List
+              </Button>
             </Box>
           </Grid>
         }
